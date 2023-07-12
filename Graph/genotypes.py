813def0049ba71071669283529251047a7ef1316
YuxinTang0315 <91824185+YuxinTang0315@users.noreply.github.com>
2023-07-12 10:53:36 +0800

    Create genotypes.py

---
+++ Graph/genotypes.py
@@ -0,0 +1,8 @@
+Graph_Transformer = ([
+                      ('GraphSage', 1),
+                      ('GCN', 2),
+                      ('Transformer', 3),
+                      ('Big_Transformer', 4),
+                      ('GINE', 5),
+                      ])
+
